083330db42fd02ace68748fb8cb2bfdf025ae5b1
ExquisiteCore <xiaolongqq1y@163.com>
2025-11-10 16:32:23 +0800

    feat: 更新CMakeLists.txt和main.cpp，添加Windows下UTF-8编码支持，解决中文乱码问题

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,18 @@
 #include <QQmlContext>
 #include "model/ChessBoardModel.h"
 
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_WIN
+    // Windows下设置控制台输出为UTF-8编码，解决中文乱码
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
